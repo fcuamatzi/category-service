817d5fbf5e1eb201d5104b8de68e499f391ee234
fjavier <fjavier.cuamatzi@gmail.com>
2022-03-22 13:24:01 -0600

    add yaml service and pods

--- src/main/java/mx/com/cuamatziinc/ventas/categoryservice/controller/ControllerCategory.java
+++ src/main/java/mx/com/cuamatziinc/ventas/categoryservice/controller/ControllerCategory.java
@@ -2,8 +2,10 @@ package mx.com.cuamatziinc.ventas.categoryservice.controller;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,12 @@ import mx.com.cuamatziinc.ventas.commonsservice.controller.ControllerCommon;
 
 @RestController
 public class ControllerCategory extends ControllerCommon<ProductCategory, IServiceCategory> {
+	
+	@Value("${mi.variable.entorno}")
+	private String miVariable;
+	
+	@Value("${node.name}")
+	private String nodeName;
 
 	@PutMapping("/{id}")
 	public ResponseEntity<?> modify(@RequestBody ProductCategory category, @PathVariable Long id) {
@@ -33,6 +41,20 @@ public class ControllerCategory extends ControllerCommon<ProductCategory, IServi
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(productCategoryDB);
 
+	}
+	
+	@GetMapping("/saludar")
+	public ResponseEntity<?> saludar(){
+		
+		return ResponseEntity.ok(miVariable);
+		
+	}
+	
+	@GetMapping("/node")
+	public ResponseEntity<?> getNodeNAme(){
+		
+		return ResponseEntity.ok(nodeName);
+		
 	}
 
 }
